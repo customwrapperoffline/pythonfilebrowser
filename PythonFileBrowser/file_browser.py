f7c8457ed17743edf17f8ab768b87b61437699a0
customwrapperoffline <136158281+customwrapperoffline@users.noreply.github.com>
2023-07-29 15:46:58 +0200

    Update file_browser.py

--- PythonFileBrowser/file_browser.py
+++ PythonFileBrowser/file_browser.py
@@ -6,105 +6,108 @@ import subprocess
 import distro
 from colorama import Fore, init
 
-def display_system_info():
-    print(Fore.YELLOW + "Python version:", platform.python_version())
-    system_info = platform.uname()
-    print("Operating system:", system_info.system, system_info.release)
 
-    if platform.system() == 'Linux':
-        dist_info = distro.linux_distribution(full_distribution_name=False)
-        dist_name = dist_info[0]
-        dist_version = dist_info[1]
-        print("Linux distribution:", dist_name, dist_version)
-    elif platform.system() == 'Windows':
-        win_version = platform.win32_ver()[0]
-        print("Windows version:", win_version)
+def display_system_info():
+  print(Fore.YELLOW + "Python version:", platform.python_version())
+  system_info = platform.uname()
+  print("Operating system:", system_info.system, system_info.release)
+
+  if platform.system() == 'Linux':
+    dist_info = distro.linux_distribution(full_distribution_name=False)
+    dist_name = dist_info[0]
+    dist_version = dist_info[1]
+    print("Linux distribution:", dist_name, dist_version)
+  elif platform.system() == 'Windows':
+    win_version = platform.win32_ver()[0]
+    print("Windows version:", win_version)
+
 
 def browse_files():
-    current_directory = os.getcwd()
+  current_directory = os.getcwd()
 
-    while True:
-        new_path = ""  # Initialize the new_path variable
-        print("\n" + Fore.CYAN + "Current directory:", current_directory)
-        print(Fore.RESET + "Files and directories in", current_directory + ":\n")
+  while True:
+    new_path = ""  # Initialize the new_path variable
+    print("\n" + Fore.CYAN + "Current directory:", current_directory)
+    print(Fore.RESET + "Files and directories in", current_directory + ":\n")
 
-        for item in os.listdir(current_directory):
-            if os.path.isdir(os.path.join(current_directory, item)):
-                print(Fore.BLUE + item)
-            else:
-                print(Fore.RESET + item)
+    for item in os.listdir(current_directory):
+      if os.path.isdir(os.path.join(current_directory, item)):
+        print(Fore.BLUE + item)
+      else:
+        print(Fore.RESET + item)
 
-        command = input(
-            "\nEnter 'back', 'd' to delete, 'c' to create folder, 't' to create/edit text file, 'k' to open a file, or file/directory name to enter (q to quit): "
-        )
+    command = input(
+        "\nEnter 'back', 'd' to delete, 'c' to create folder, 't' to create/edit text file, 'k' to open a file, or file/directory name to enter (q to quit): "
+    )
 
-        if command == 'q':
-            break
-        elif command == 'back':
-            current_directory = os.path.dirname(current_directory)
-        elif command == 'c':
-            new_folder = input("Enter folder name to create: ")
-            new_folder_path = os.path.join(current_directory, new_folder)
-            os.makedirs(new_folder_path, exist_ok=True)
-            print(Fore.GREEN + "Folder created:", new_folder_path)
-        elif command == 't':
-            file_name = input("Enter text file name to create/edit: ")
-            file_path = os.path.join(current_directory, file_name)
+    if command == 'q':
+      break
+    elif command == 'back':
+      current_directory = os.path.dirname(current_directory)
+    elif command == 'c':
+      new_folder = input("Enter folder name to create: ")
+      new_folder_path = os.path.join(current_directory, new_folder)
+      os.makedirs(new_folder_path, exist_ok=True)
+      print(Fore.GREEN + "Folder created:", new_folder_path)
+    elif command == 't':
+      file_name = input("Enter text file name to create/edit: ")
+      file_path = os.path.join(current_directory, file_name)
 
-            with open(file_path, 'w') as file:
-                file_content = input("Enter the text content:\n")
-                file.write(file_content)
+      with open(file_path, 'w') as file:
+        file_content = input("Enter the text content:\n")
+        file.write(file_content)
 
-            print(Fore.GREEN + "Text file created/updated:", file_path)
-        elif command == 'd':
-            delete_item = input("Enter file/directory name to delete: ")
-            delete_path = os.path.join(current_directory, delete_item)
-            if os.path.exists(delete_path):
-                if os.path.isfile(delete_path):
-                    os.remove(delete_path)
-                    print(Fore.RED + "File deleted:", delete_path)
-                else:
-                    shutil.rmtree(delete_path)
-                    print(Fore.RED + "Folder deleted:", delete_path)
-            else:
-                print(Fore.RED + "File/Folder not found.")
-        elif command == 'k':
-            file_name = input("Enter the text file name to open: ")
-            file_path = os.path.join(current_directory, file_name)
-            if os.path.isfile(file_path):
-                if platform.system() == 'Windows':
-                    os.startfile(file_path)
-                elif platform.system() == 'Darwin':  # For macOS
-                    subprocess.Popen(['open', file_path])
-                elif platform.system() == 'Linux':
-                    subprocess.Popen(['xdg.open', file_path])
-                else:
-                    print("Opening files is not supported on this operating system.")
-            else:
-                print(Fore.RED + "File not found.")
+      print(Fore.GREEN + "Text file created/updated:", file_path)
+    elif command == 'd':
+      delete_item = input("Enter file/directory name to delete: ")
+      delete_path = os.path.join(current_directory, delete_item)
+      if os.path.exists(delete_path):
+        if os.path.isfile(delete_path):
+          os.remove(delete_path)
+          print(Fore.RED + "File deleted:", delete_path)
         else:
-            new_path = os.path.join(current_directory, command)
+          shutil.rmtree(delete_path)
+          print(Fore.RED + "Folder deleted:", delete_path)
+      else:
+        print(Fore.RED + "File/Folder not found.")
+    elif command == 'k':
+      file_name = input("Enter the text file name to open: ")
+      file_path = os.path.join(current_directory, file_name)
+      if os.path.isfile(file_path):
+        if platform.system() == 'Windows':
+          os.startfile(file_path)
+        elif platform.system() == 'Darwin':  # For macOS
+          subprocess.Popen(['open', file_path])
+        elif platform.system() == 'Linux':
+          subprocess.Popen(['xdg.open', file_path])
+        else:
+          print("Opening files is not supported on this operating system.")
+      else:
+        print(Fore.RED + "File not found.")
+    else:
+      new_path = os.path.join(current_directory, command)
 
-            if os.path.isfile(new_path):
-                if new_path.endswith('.txt'):
-                    with open(new_path, 'r') as file:
-                        content = file.read()
-                        print("\nContent of", command + ":\n")
-                        print(Fore.RESET + content)
-                    edit = input("Do you want to edit this file? (y/n): ").lower()
-                    if edit == 'y':
-                        with open(new_path, 'w') as file:
-                            new_content = input("Enter the new content:\n")
-                            file.write(new_content)
-                        print(Fore.GREEN + "Text file updated:", new_path)
-                else:
-                    print(Fore.RED + command, "is not a text file.")
-            elif os.path.isdir(new_path):
-                current_directory = new_path
-            else:
-                print(Fore.RED + "Invalid file/directory name. Try again.")
+      if os.path.isfile(new_path):
+        if new_path.endswith('.txt'):
+          with open(new_path, 'r') as file:
+            content = file.read()
+            print("\nContent of", command + ":\n")
+            print(Fore.RESET + content)
+          edit = input("Do you want to edit this file? (y/n): ").lower()
+          if edit == 'y':
+            with open(new_path, 'w') as file:
+              new_content = input("Enter the new content:\n")
+              file.write(new_content)
+            print(Fore.GREEN + "Text file updated:", new_path)
+        else:
+          print(Fore.RED + command, "is not a text file.")
+      elif os.path.isdir(new_path):
+        current_directory = new_path
+      else:
+        print(Fore.RED + "Invalid file/directory name. Try again.")
+
 
 if __name__ == "__main__":
-    init(autoreset=True)
-    display_system_info()
-    browse_files()
+  init(autoreset=True)
+  display_system_info()
+  browse_files()
